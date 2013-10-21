d3a59c16903a9c5658ef1204c0ab0a2c7f401d90
James Casey <jamkcas@gmail.com>
2013-10-21 10:07:29 -0700

    Added user model, users and sessions controller, got the sign-up, log-in, log-out, and change password functionality working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  skip_before_filter :authorize, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+      @user = current_user
+
+  end
+
+  def create
+    @user = User.create(params[:user])
+    if @user.errors.empty?
+      sign_in(@user)
+      redirect_to user_path(@user.id)
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update_attributes(params[:user])
+    if @user.errors.empty?
+      sign_in(@user)
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  attr_accessible :user_name, :email, :password, :password_confirmation
+
+  before_save :create_remember_token
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 8}
+  validates :password_confirmation, presence: true
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
+end
