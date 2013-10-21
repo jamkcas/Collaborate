380daa6ee8bd2e87c1951d5aaf9474009920a6e1
James Casey <jamkcas@gmail.com>
2013-10-20 18:09:15 -0700

    Start of the Collaborate Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collaborate::Application.config.secret_token = '2dbba0681951cb1efa81e9f7b78e8eb9f37767348423db1fc4c281794f311bb26817b6e8b06c172ce48985cb94078e0a33d1f52228aa286363560bc58ff0fd6c'
